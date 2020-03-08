8b07babc8cbc1ae842aaf4c1f5cb769867070f56
Calin Ilie <calin@ilie.io>
2020-03-08 00:58:52 +0000

    Fix all deploy scripts

--- deploy/scripts/dev/install.sh
+++ deploy/scripts/dev/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 helm install draco ./deploy/helm/draco \
   --values ./deploy/helm/values/dev/values.yaml \
+  --set timestamp="$(date)" \
   --namespace dev
--- deploy/scripts/prod/install.sh
+++ deploy/scripts/prod/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 helm install draco ./deploy/helm/draco \
   --values ./deploy/helm/values/prod/values.yaml \
+  --set timestamp="$(date)" \
   --namespace default
